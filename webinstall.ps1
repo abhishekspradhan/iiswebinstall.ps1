dfa29a4d6c8d7c640f89e06f24f92af60e5bf173
abhishekspradhan <69381187+abhishekspradhan@users.noreply.github.com>
2021-08-27 21:47:51 +0530

    Update webinstall.ps1

--- webinstall.ps1
+++ webinstall.ps1
@@ -1,3 +1,3 @@
 Import-Module Servermanager
-Install-WindowsFeature -Name "Web-Server" -IncludeAllSubFeature
+Install-WindowsFeature -Name "Web-Server" -IncludeManagementTools
 Set-Content -Path "C:\inetpub\wwwroot\Default.html" -Value "This is the server: $($env:COMPUTERNAME)!"
